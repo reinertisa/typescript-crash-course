e25789a5f2c006c51bc58c9ac55901b19fb1cb96
Isa Reinert <reinert.isa@gmail.com>
2025-05-11 14:05:25 -0700

    feat(ts): add type basics

---
+++ index.ts
@@ -0,0 +1,21 @@
+let age: number = 30;
+let firstName: string = 'Sade';
+let isLoggedIn: boolean = true;
+
+console.log(age, firstName, isLoggedIn);
+
+age = 40;
+firstName = 'Isa';
+isLoggedIn = false;
+
+console.log(age, firstName, isLoggedIn);
+
+
+// null & undefined
+let something: null;
+let somethingElse: undefined;
+
+something = null;
+somethingElse = undefined;
+
+console.log(something, somethingElse);
